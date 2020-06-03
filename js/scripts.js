75f53ac068928fb54e03c17c340a837b0d6ef4d0
Mariel Hamson <mariel.hamson@gmail.com>
2020-06-03 10:17:09 -0700

    add html for basic shouting site, add js for form submission and uppercase conversion
    Co-Authored-by: Nikolas Kososik <nikolas.kososik@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("#form1").submit(function(event){
+    const input=$("input#sentence").val().toUpperCase();
+    $("#words").text(input);
+
+  $(".response").show();
+
+  event.preventDefault();
+  })
+});
